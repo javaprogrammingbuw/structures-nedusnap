7f846bf37db4d4d0d804d71736f6d42132a10bee
Godfrey Chinedu Victor <godfrey97@yahoo.com>
2019-10-20 18:55:18 +0100

    homework (1)

--- EvenOdd.java
+++ EvenOdd.java
@@ -10,6 +10,24 @@ public class EvenOdd {
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
+
+		//using a for loop
+
+		for (int i=0; i<=n; i++) {
+			if(i%2 == 0)
+				System.out.println(i);
+		}
+
+
+		//using a while loop
+
+/*		int i = 0;
+		while (i<=n) {
+			if(i%2 == 1)
+				System.out.println(i);
+		}
+		i++;
+*/
 	}
 	
 }
