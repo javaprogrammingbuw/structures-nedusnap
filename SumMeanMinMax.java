429008b73228864be5a1bded508b7ef7ef5d6b78
Godfrey Chinedu Victor <godfrey97@yahoo.com>
2019-10-20 18:59:11 +0100

    homework(2)

--- SumMeanMinMax.java
+++ SumMeanMinMax.java
@@ -19,6 +19,34 @@ public class SumMeanMinMax {
 		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
 		 * */
 		
+		//Sum
+		int sum = a + b + c;
+		System.out.println("The sum of a, b and c is " + sum);
+
+		//Mean
+		int mean = (a+b+c)/3;
+		System.out.println("The mean of a, b and c is " + mean);
+
+		//Max Value
+		int maxValue = 0;
+		if(a > maxValue) maxValue = a;
+
+		if(b > maxValue) maxValue = b;
+
+		if(c > maxValue) maxValue = c;
+
+		System.out.println("The maximum value of a, b, and c is " + maxValue);
+
+		//Min Value
+		int minValue = 5000;
+		if (a < minValue) minValue = a;
+
+		if (b < minValue) minValue = b;
+
+		if (c < minValue) minValue = c;
+
+		System.out.println("The minimum value of a, b and c is " + minValue);
+
 
 	}
 
